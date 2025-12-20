4d02eacbc55ccea743dffbfc8e088f27833b111b
Vladislav Bushunov <vladbushunov@gmail.com>
2025-12-20 08:05:17 +0000

    station dimension

---
+++ models/station_dimension.sql
@@ -0,0 +1,15 @@
+
+with bike_station as
+(
+    select distinct
+        start_statio_id as station_id
+        , start_station_name as station_name
+        , start_lat as station_lat
+        , start_lng as start_station_lng
+    from {{ source('DEMO', 'BIKE') }}
+    where ride_id != 'ride_id'
+
+)
+
+select *
+from bike_station
